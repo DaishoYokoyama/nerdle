f4d74d7e1e8db4e6d5c6a5e2350cc2bf5ca3c9fb
Daisho Yokoyama <daisyou117@gmail.com>
2023-02-06 23:58:39 +0900

    feat: pwa support

---
+++ packages/web/public/serviceWorker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html", "offline.html"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const casheWhitelist = [];
+  casheWhitelist.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((casheNames) =>
+      Promise.all(
+        casheNames.map((casheName) => {
+          if (!casheWhitelist.includes(casheName)) {
+            return cashes.delete(casheName);
+          }
+        })
+      )
+    )
+  );
+});
