217f27fd6fa290553ec6f664c60641fbd67767b6
Daisho Yokoyama <daisyou117@gmail.com>
2023-02-08 19:56:10 +0900

    refactor: frontend model

--- packages/web/src/api/rest/domain/index.ts
+++ packages/web/src/api/rest/domain/index.ts
@@ -1,5 +1,50 @@
-export * from "./rule";
-export * from "./session";
-export * from "./box";
-export * from "./key";
-export * from "./guess";
+/*****************************************************
+ * This file is generated from the API specification.
+ *****************************************************/
+
+export type Color = "green" | "dark" | "violet";
+
+export type DataAccessObject = {
+  id: string;
+  createdAt: string;
+};
+
+export type Box = {
+  id: string;
+  value?: string;
+  group: string;
+  color?: Color;
+};
+
+export type Key = DataAccessObject & {
+  value: string;
+  type: "number" | "operator";
+  color?: Color;
+};
+
+export type Session = {
+  id: string;
+  ruleId: string;
+  attempt: number;
+  attemptLimits: number;
+  correctValueLength: number;
+  boxes: Box[];
+  keys: Key[];
+};
+
+export type CreateSessionRequest = {
+  ruleId?: string;
+};
+
+export type CreateSessionResponse = Session;
+
+export type ExecuteGuessRequest = {
+  ruleId: string;
+  boxes: Box[];
+};
+
+export type ExecuteGuessResponse = {
+  ruleId: string;
+  boxes: Box[];
+  isCorrect: boolean;
+};
